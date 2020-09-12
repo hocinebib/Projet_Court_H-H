04fc26ee7b124b7b2cf1807a2c59feaee0dcb342
hocinebib <hocine.meraouna@gmail.com>
2020-09-12 12:02:16 +0200

    added function that creates the spheres

---
+++ src/sphere_pts.py
@@ -0,0 +1,46 @@
+#! /usr/bin/python3
+"""
+
+program that 
+
+"""
+import numpy as np
+
+VDW_RADIUS = {'H':1.20, 'C':1.7, 'N':1.55, 'O':1.52, 'CL':1.75, 'F':1.47, 'P':1.80, 
+'S':1.80, 'CU':1.40, 'HE':1.40, 'LI':1.82, 'BE':1.53, 'B':1.92, 'NE':1.54, 'NA':2.27,
+'MG':1.73, 'AL':1.84, 'SI':2.10, 'AR':1.88, 'K':2.75, 'CA':2.31, 'SC':2.11, 'NI':1.63,
+'ZN':1.39, 'GA':1.87, 'GE':2.11, 'AS':1.85, 'SE':1.90, 'BR':1.85, 'KR':2.02, 'RB':3.03,
+'SR':2.49, 'PD':1.63, 'AG':1.72, 'CD':1.58, 'IN':1.93, 'SN':2.17, 'SB':2.06, 'TE':2.06,
+'I':1.98, 'XE':2.16, 'CS':3.43, 'BA':2.68, 'PT':1.75, 'AU':1.66, 'HG':1.55, 'TL':1.96, 
+'PB':2.02, 'BI':2.07, 'PO':1.97, 'AT':2.02, 'RN':2.20, 'FR':3.48, 'RA':2.83, 'U':1.86}
+#https://www.elementschimiques.fr/?fr/proprietes/chimiques/rayon-de-van-der-waals
+
+def spheres(atoms_coor, n):
+	"""
+	atoms_coor : the list that arom_coor returns
+	n : the number of points distributed on the sphere
+	golden angle algorithm
+	"""
+
+	sphere_lst = []
+
+	golden_angle = np.pi * (3 - np.sqrt(5))
+	
+	for atom in atoms_coor:
+
+		sphere_lst.append(atom[0])
+
+		center = atom[1]
+		theta = golden_angle * np.arange(n)
+		z = np.linspace(1 - 1.0 / n, 1.0 / n - 1, n)
+
+		radius = VDW_RADIUS[atom[0]] + VDW_RADIUS['O']
+
+		points = numpy.zeros((n, 3))
+		points[:,0] = radius * numpy.cos(theta) + center[0]
+		points[:,1] = radius * numpy.sin(theta) + center[1]
+		points[:,2] = z + center[2]
+
+		sphere_lst.append(points)
+	
+	return sphere_lst
